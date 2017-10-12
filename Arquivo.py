3e7aacb15ef1b42471fd304bb0cee82c874704c3
maykon <maykonoliveira@edu.univali.br>
2017-10-12 16:36:15 -0300

    Adicionado status à classe

--- Arquivo.py
+++ Arquivo.py
@@ -1,9 +1,10 @@
 class Arquivo(object):
 
-    def __init__(self,nome="",diretorio="",hash=""):
+    def __init__(self,nome="",diretorio="",hash="",status=False):
         self.__nome = nome
         self.__diretorio = diretorio
         self.__hash = hash
+        self.__status = status
 
     def obterNome(self):
         return self.__nome
@@ -22,3 +23,9 @@ class Arquivo(object):
 
     def definirHash(self,hash):
         self.__hash = hash
+
+    def obterStatus(self):
+        return self.__status
+
+    def definirStatus(self,status):
+        self.__status=status
