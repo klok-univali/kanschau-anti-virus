09401803b8def1bb2a9e46df52c260ffd6b646b8
Gabriel H Klok <gabrielhklok@openmailbox.org>
2017-10-12 19:50:48 -0300

    Arquivos Analise

---
+++ analise/Analise.py
@@ -0,0 +1,12 @@
+class Analise():
+
+    def __init__(self):
+        pass
+
+    def itemQuarentena(self):
+        pass
+        return
+
+    def itemIgnorado(self):
+        pass
+        return
