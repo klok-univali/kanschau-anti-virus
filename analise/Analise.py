42ea8415bb63f0cd072eaa7c667dddca6b323694
Gabriel H Klok <gabrielhklok@openmailbox.org>
2017-10-12 22:00:46 -0300

    continuacao analise

--- analise/Analise.py
+++ analise/Analise.py
@@ -1,7 +1,23 @@
+import hashlib
+
 class Analise():
 
-    def __init__(self):
-        pass
+    def __init__(self, listaArqLidos):
+        h = hashlib.md5()
+
+        for arqLido in listaArqLidos:
+            file = open(arqLido)
+            h.update(file.read().encode())
+            #print(h.hexdigest())
+
+            if self.itemQuarentena() or self.itemIgnorado():
+                continue
+
+            
+
+
+
+
 
     def itemQuarentena(self):
         pass
@@ -10,3 +26,4 @@ class Analise():
     def itemIgnorado(self):
         pass
         return
+
