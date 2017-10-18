302a5fdb16cc01be68790f698d8d2a63f69920ee
Gabriel H Klok <gabrielhklok@openmailbox.org>
2017-10-18 21:43:52 -0200

    principal

--- Principal.py
+++ Principal.py
@@ -4,18 +4,30 @@ from persistencia.Armazenamento import Armazenamento
 from persistencia.Serializacao import Serializacao
 from modelos.Arquivo import Arquivo
 from modelos.Quarentena import Quarentena
+import os
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from layout.layout import Ui_Kanschau
 
 #--------------------------------------------------------#
 
-varredura = Varredura()
-quarentena = Quarentena()
+# varredura = Varredura()
+# quarentena = Quarentena()
+#
+# varredura.capturarArquivos("/home/srfr33dy/Documentos/")
+#
+# analise = Analise(varredura.obterArquivosCapturados(),quarentena)
+#
+# for arquivo in quarentena.obterArquivosQuarentena():
+#
+#     print(arquivo.obterNome())
 
-varredura.capturarArquivos("/home/srfr33dy/Documentos/")
-
-analise = Analise(varredura.obterArquivosCapturados(),quarentena)
-
-for arquivo in quarentena.obterArquivosQuarentena():
-
-    print(arquivo.obterNome())
+#  print(os.system("echo $HOME"))
 
 
+app = QApplication([])
+window = QMainWindow()
+main_window = Ui_Kanschau()
+main_window.setupUi(window)
+window.show()
+app.exec_()
