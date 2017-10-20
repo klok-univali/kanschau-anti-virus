e00fb38d68e0e93c4ef9185e98ecd9965187debb
Gabriel H Klok <gabrielhklok@hotmail.com>
2017-10-20 08:54:03 -0200

    Correção analise

--- analise/Analise.py
+++ analise/Analise.py
@@ -3,28 +3,30 @@ from modelos.Arquivo import Arquivo
 
 class Analise():
 
-    def __init__(self, listaArquivos,quarentena):
+    def __init__(self):
+        pass
+
+    def analisar(self, arquivo, quarentena):
         baseVirus = open("bases/Base_Virus",'r')
 
-        for arquivo in listaArquivos:
+        pipe = os.popen("md5sum -b " + arquivo + "|cut -f1 -d\' \'")
+        hash = pipe.read().replace('\n','')
+        hash = hash.replace(")","\)")
+        pipe.close()
 
-            pipe = os.popen("md5sum -b " + arquivo + "|cut -f1 -d\' \'")
-            hash = pipe.read().replace('\n','')
-            pipe.close()
+        if self.itemQuarentena(hash,quarentena.obterArquivosQuarentena()) or self.itemIgnorado(hash,quarentena.obterArquivosIgnorados()):
+            return
 
-            if self.itemQuarentena(hash,quarentena.obterArquivosQuarentena()) or self.itemIgnorado(hash,quarentena.obterArquivosIgnorados()):
-                continue
+        for hashVirus in baseVirus.readlines():
 
-            for hashVirus in baseVirus.readlines():
+            if hash in hashVirus.replace('\n',''):
+                objetoArquivo = Arquivo()
+                objetoArquivo.definirNome(os.path.basename(arquivo))
+                objetoArquivo.definirDiretorio(os.path.dirname(arquivo)+'/')
+                objetoArquivo.definirHash(hash)
+                quarentena.adicionarArquivoQuarentena(objetoArquivo)
 
-                if hash in hashVirus.replace('\n',''):
-                    objetoArquivo = Arquivo()
-                    objetoArquivo.definirNome(os.path.basename(arquivo))
-                    objetoArquivo.definirDiretorio(os.path.dirname(arquivo)+'/')
-                    objetoArquivo.definirHash(hash)
-                    quarentena.adicionarArquivoQuarentena(objetoArquivo)
-
-            baseVirus.seek(0,0)
+        baseVirus.seek(0,0)
 
     def itemQuarentena(self,hash,quarentena):
 
